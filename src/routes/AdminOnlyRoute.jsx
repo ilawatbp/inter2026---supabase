0080af9cfd3820338d3e3ffb678530672c7bbc62
niko sarmiento <nksarmiento1@gmail.com>
2026-04-28 08:52:17 +0800

    otp fixed

--- src/routes/AdminOnlyRoute.jsx
+++ src/routes/AdminOnlyRoute.jsx
@@ -3,28 +3,35 @@ import { useAuth } from "../context/AuthContext";
 import { ThreeDot } from "react-loading-indicators";
 
 export default function AdminOnlyRoute() {
-  const { loading, profileLoading, isAuthenticated, isAdmin, otpPending } = useAuth();
+const {
+  isAuthenticated,
+  isAdmin,
+  loading,
+  profileLoading,
+  deviceChecking,
+  otpPending,
+} = useAuth();
   const location = useLocation();
 
-  if (loading || profileLoading) {
-    return (
-      <div className="min-h-dvh w-full bg-white flex items-center justify-center">
-        <ThreeDot color="#2f963c" size="medium" text="" textColor="" />
-      </div>
-    );
-  }
+if (loading || profileLoading || deviceChecking) {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+     <ThreeDot color="#2f963c" size="medium" text="" textColor="" />
+    </div>
+  );
+}
 
-  if (!isAuthenticated) {
-    return <Navigate to="/login" replace state={{ from: location }} />;
-  }
+if (!isAuthenticated) {
+  return <Navigate to="/login" replace state={{ from: location }} />;
+}
 
-  if (otpPending) {
-    return <Navigate to="/login" replace state={{ from: location }} />;
-  }
+if (otpPending) {
+  return <Navigate to="/login" replace state={{ from: location }} />;
+}
 
-  if (!isAdmin) {
-    return <Navigate to="/" replace />;
-  }
+if (!isAdmin) {
+  return <Navigate to="/" replace />;
+}
 
   return <Outlet />;
 }
--- src/routes/UserOnlyRoute.jsx
+++ src/routes/UserOnlyRoute.jsx
@@ -3,16 +3,23 @@ import { useAuth } from "../context/AuthContext";
 import { ThreeDot } from "react-loading-indicators";
 
 export default function UserOnlyRoute() {
-  const { loading, profileLoading, isAuthenticated, isAdmin, otpPending } = useAuth();
+const {
+  isAuthenticated,
+  loading,
+  profileLoading,
+  deviceChecking,
+  isAdmin,
+  otpPending,
+} = useAuth();
   const location = useLocation();
 
-  if (loading || profileLoading) {
-    return (
-      <div className="min-h-dvh w-full bg-white flex items-center justify-center">
-        <ThreeDot color="#2f963c" size="medium" text="" textColor="" />
-      </div>
-    );
-  }
+if (loading || profileLoading || deviceChecking) {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <ThreeDot color="#2f963c" size="medium" text="" textColor="" />
+    </div>
+  );
+}
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace state={{ from: location }} />;
